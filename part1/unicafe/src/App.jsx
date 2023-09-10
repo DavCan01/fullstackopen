6139ba994fcd3209aab2e39747e006b72d660891
DavCan01 <79264762+DavCan01@users.noreply.github.com>
2023-09-10 20:00:42 +0200

    1.7 unicafe - Add more statistics

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,38 +8,49 @@ const Button = ({ onClick, text }) => (
   </button>
 )
 
-const Statistic = ({text, number }) => (
-  <p> { text } {number} </p>
-)
-
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handleSetGood = () => {
-    setGood(good + 1)
-  }
-  const handleSetNeutral = () => {
-    setNeutral(neutral + 1)
-  }
-  const handleSetBad = () => {
-    setBad(bad + 1)
+  const total = () => good + bad + neutral;
+  const average = () => (good - bad) / total();
+  const positive = () => (good / total()) * 100;
+
+  const increaseFeedback = (statistic) => {
+
+    switch (statistic) {
+      case "good":
+        setGood(good => good + 1);
+      break;
+
+      case "neutral":
+        setNeutral(neutral => neutral + 1);
+      break;
+
+      case "bad":
+        setBad(bad => bad + 1);
+      break;
+
+    }
   }
 
   return (
     <div>
       <h1> Give feedback </h1>
-      <Button onClick={handleSetGood} text='good' />
-      <Button onClick={handleSetNeutral} text='neutral' />
-      <Button onClick={handleSetBad} text='bad' />
+      <Button onClick={() => increaseFeedback("good")} text='good' />
+      <Button onClick={() => increaseFeedback("neutral")} text='neutral' />
+      <Button onClick={() => increaseFeedback("bad")} text='bad' />
 
       <h1> Statistics </h1>
-      <Statistic text = 'good' number = {good} ></Statistic>
-      <Statistic text = 'neutral' number = {neutral} ></Statistic>
-      <Statistic text = 'bad' number = {bad} ></Statistic>
+      <p> good {good} </p>
+      <p> neutral {neutral} </p>
+      <p> bad {bad} </p>
 
+      <p> all {total()} </p>
+      <p> average {average()} </p>
+      <p> positive {positive()} % </p>
     </div>
   )
 }
